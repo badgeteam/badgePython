2b524a515c935182cab1ccd5f45182914c648e6d
Joris Witteman <joriswitteman@hotmail.com>
2021-03-31 15:43:45 +0200

    Increased heap to 80k

--- firmware/components/micropython/main.c
+++ firmware/components/micropython/main.c
@@ -105,6 +105,7 @@ void mp_task(void *pvParameter) {
     #else
     // Allocate the uPy heap using malloc and get the largest available region
     size_t mp_task_heap_size = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT);
+    mp_task_heap_size = 80*1024;
     void *mp_task_heap = malloc(mp_task_heap_size);
     #endif
 
