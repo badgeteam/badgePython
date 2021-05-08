aa64815f25f38b1faf7e36afdcc21417d6aa76f1
Joris Witteman <joriswitteman@hotmail.com>
2021-05-08 11:15:04 +0200

    Added fsoverbus support

---
+++ firmware/components/driver_fsoverbus/specialfunctions.c
@@ -0,0 +1,30 @@
+#include "include/packetutils.h"
+#include "include/specialfunctions.h"
+#include "driver_rtcmem.h"
+#include <esp_sleep.h>
+#include <esp_err.h>
+#include <esp_log.h>
+
+#define TAG "fsoveruart_sf"
+
+int execfile(uint8_t *data, uint16_t command, uint32_t message_id, uint32_t size, uint32_t received, uint32_t length) {
+    if(received != size) return 0;
+
+    ESP_LOGI(TAG, "Starting: %s", data);
+    sendok(command, message_id);
+    driver_rtcmem_string_write((char*) data);
+    esp_deep_sleep(1000000);
+    return 1;
+}
+
+int heartbeat(uint8_t *data, uint16_t command, uint32_t message_id, uint32_t size, uint32_t received, uint32_t length) {
+    if(received != size) return 0;
+    sendok(command, message_id);
+    return 1;
+}
+
+//Old function used in CZ20, currently unsupported. Function still exists to prevent reuse of the command.
+int pythonstdin(uint8_t *data, uint16_t command, uint32_t message_id, uint32_t size, uint32_t received, uint32_t length) {
+    sendok(command, message_id);
+    return 1;
+}
