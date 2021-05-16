1e114c3ef07decbc0c808710dc1f19d5e333086c
Joris Witteman <joriswitteman@hotmail.com>
2021-05-16 15:24:44 +0200

    Fixed SH1106 support

--- firmware/components/driver_framebuffer/include/driver_framebuffer_devices.h
+++ firmware/components/driver_framebuffer/include/driver_framebuffer_devices.h
@@ -62,12 +62,12 @@
 	#define COLOR_TEXT_DEFAULT 0xFFFFFF
 
 #elif defined(CONFIG_DRIVER_SH1106_ENABLE)
-	#define FB_SIZE SSD1306_BUFFER_SIZE
-	#define FB_WIDTH SSD1306_WIDTH
-	#define FB_HEIGHT SSD1306_HEIGHT
+	#define FB_SIZE SH1106_BUFFER_SIZE
+	#define FB_WIDTH SH1106_WIDTH
+	#define FB_HEIGHT SH1106_HEIGHT
 	#define FB_TYPE_1BPP
 	#define FB_1BPP_VERT
-	#define FB_FLUSH(buffer,eink_flags,x0,y0,x1,y1) driver_ssd1306_write_part(buffer,x0,y0,x1,y1)
+	#define FB_FLUSH(buffer,eink_flags,x0,y0,x1,y1) driver_sh1106_write_part(buffer,x0,y0,x1,y1)
 	#define COLOR_FILL_DEFAULT 0x000000
 	#define COLOR_TEXT_DEFAULT 0xFFFFFF
 
