c405c2e81712b567ec78a1a21e993c28b9512ab1
Joris Witteman <joriswitteman@hotmail.com>
2022-05-14 11:37:14 +0200

    Added handler

--- firmware/components/driver_mch22_rp2040/modrp2040.c
+++ firmware/components/driver_mch22_rp2040/modrp2040.c
@@ -6,10 +6,15 @@
 #include "py/runtime.h"
 #endif
 
+#define TAG "RP2040_UPY"
+
 #define RP2040_ADDR 0x17
 #define GPIO_INT_RP2040  34
 
 static RP2040 rp2040;
+static mp_obj_t touch_callback = mp_const_none;
+
+
 
 void driver_rp2040_init() {
     rp2040.i2c_bus = 0;
@@ -25,6 +30,24 @@ static mp_obj_t buttons() {
     return mp_obj_new_int(value);
 }
 static MP_DEFINE_CONST_FUN_OBJ_0(buttons_obj, buttons);
+
+static void button_handler(uint16_t touch_state) {
+    if (touch_callback != mp_const_none) {
+        // uPy scheduler tends to get full for unknown reasons, so to not lose any interrupts,
+        // we try until the schedule succeeds.
+        bool succeeded = mp_sched_schedule(touch_callback, mp_obj_new_int(touch_state));
+        while (!succeeded) {
+            ESP_LOGW(TAG, "Failed to call touch callback, retrying");
+            succeeded = mp_sched_schedule(touch_callback, mp_obj_new_int(touch_state));
+        }
+    }
+}
+
+static mp_obj_t set_handler(mp_obj_t handler) {
+    touch_callback = handler;
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(set_handler_obj, set_handler);
 
 static mp_obj_t get_brightness() {
     uint8_t value;
@@ -44,6 +67,7 @@ STATIC const mp_rom_map_elem_t rp2040_module_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR_buttons), MP_ROM_PTR(&buttons_obj)},
     {MP_ROM_QSTR(MP_QSTR_get_brightness), MP_ROM_PTR(&get_brightness_obj)},
     {MP_ROM_QSTR(MP_QSTR_set_brightness), MP_ROM_PTR(&set_brightness_obj)},
+    {MP_ROM_QSTR(MP_QSTR_set_handler), MP_ROM_PTR(&set_handler_obj)},
 };
 
 STATIC MP_DEFINE_CONST_DICT(rp2040_module_globals, rp2040_module_globals_table);
