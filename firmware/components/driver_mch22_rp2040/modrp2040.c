b1386a7976ddf9046f36b6ad9d4a1541049b31db
Joris Witteman <joriswitteman@hotmail.com>
2022-05-14 09:51:45 +0200

    Added support for setting and getting display brightness

--- firmware/components/driver_mch22_rp2040/modrp2040.c
+++ firmware/components/driver_mch22_rp2040/modrp2040.c
@@ -26,8 +26,24 @@ static mp_obj_t buttons() {
 }
 static MP_DEFINE_CONST_FUN_OBJ_0(buttons_obj, buttons);
 
+static mp_obj_t get_brightness() {
+    uint8_t value;
+    rp2040_get_lcd_backlight(&rp2040, &value);
+    return mp_obj_new_int(value);
+}
+static MP_DEFINE_CONST_FUN_OBJ_0(get_brightness_obj, get_brightness);
+
+static mp_obj_t set_brightness(mp_obj_t brightness) {
+    uint8_t value = mp_obj_get_int(brightness);
+    rp2040_set_lcd_backlight(&rp2040, value);
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(set_brightness_obj, set_brightness);
+
 STATIC const mp_rom_map_elem_t rp2040_module_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR_buttons), MP_ROM_PTR(&buttons_obj)},
+    {MP_ROM_QSTR(MP_QSTR_get_brightness), MP_ROM_PTR(&get_brightness_obj)},
+    {MP_ROM_QSTR(MP_QSTR_set_brightness), MP_ROM_PTR(&set_brightness_obj)},
 };
 
 STATIC MP_DEFINE_CONST_DICT(rp2040_module_globals, rp2040_module_globals_table);
