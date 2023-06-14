20d43af865f300da04dc80ec7b64fd39dfd3ff84
Brian 'redbeard' Harrington <redbeard@dead-city.org>
2023-06-14 16:58:19 -0700

    micropython/cmake: HACK clean up component
    This is step one of a terrible hack of just flensing out code from the
    MicroPython component.  At the moment the build still doesn't work, but
    I'm mostly sure thats a matter of new-incompatibilities with
    BadgePython's opinionation around NVS.

--- components/micropython/help.c
+++
@@ -1,7 +0,0 @@
-#include "py/builtin.h"
-
-const char esp32_help_text[] =
-    "Welcome to BadgePython!\n"
-    "\n"
-    "Please visit https://docs.badge.team/ for documentation and a getting started guide.\n"
-    "\n";
